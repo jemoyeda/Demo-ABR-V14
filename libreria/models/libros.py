3ff1b053b7cb908d50e48388dc4c0409bda210dd
Ing. Hermes Colina <79210037+HermesColina2909@users.noreply.github.com>
2022-05-04 11:13:14 -0400

    libreria_10

--- libreria/models/libros.py
+++ libreria/models/libros.py
@@ -4,15 +4,16 @@ from odoo import models, fields, api
 class Libros(models.Model):
     _name = 'libros'
     
-
+#Campos del modulo creado
     name = fields.Char(string="Nombre del libro", required=True)
     editorial = fields.Char(string="editorial", required=True)
     isbn = fields.Char(string="ISBN", required=True)
     autor_id = fields.Many2one(comodel_name='autor', string="Autor", required=True)
     lastname_autor = fields.Char(related='autor_id.last_name', string="Apellido del autor")
     imagen = fields.Binary(string="Imagen")
-    description = fields.Char(string="Descripcion", computed="_compute_description")
-    
+    description = fields.Char(string="Descripcion", compute="_compute_description")
+
+#Funciones de campos calculados    
     @api.depends()
     def _compute_description(self):
         self.description = self.name + "de la " + self.editorial + "realizado por el autor " + self.lastname_autor
